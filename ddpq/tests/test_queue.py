8d4040d7b90ad1bad29e33d347cb207311d428fe
Nate Woods <big.nate.w@gmail.com>
2015-05-10 12:37:04 -0600

    Pushing 100% test coverage

--- ddpq/tests/test_queue.py
+++ ddpq/tests/test_queue.py
@@ -25,7 +25,6 @@ def test_priority_static():
         x = DDPQ()
         for item in given:
             x.push(item, 'Item {}'.format(item))
-        print x._queue
         assert len(x._queue) == 3
         result = [x.pop()[0] for _ in xrange(len(x._queue))]
         assert result == output, 'Results do not match suggested output'
@@ -37,7 +36,7 @@ def test_priority_dynamic():
     def test(i):
         x = DDPQ()
         # Generate
-        for j in xrange(int(4000)):
+        for j in xrange(4000):
             x.push(random(), 'Item {}'.format(j))
         # Verify
         last, count = x.pop(), 0
@@ -48,8 +47,17 @@ def test_priority_dynamic():
             last = x.pop()
             count += 1
 
-    for i in xrange(10):
+    for i in xrange(5):
         yield test, i
+
+def test_spool_iter_to_file():
+    # TODO: improve this test
+    x = DDPQ()
+    def item_iter():
+        for i in xrange(1000):
+            yield i, 'Item {}'.format(i)
+    result = x._spool_iter_to_file(item_iter())
+    assert result.tell() == 25780
 
 def test_length():
     x = DDPQ()
@@ -57,3 +65,41 @@ def test_length():
     for _ in xrange(length):
         x.push(random(), 'Item')
     assert len(x) == length, 'Testing length functionality'
+
+def test_peek():
+    def test(with_data):
+        x = DDPQ()
+        if with_data:
+            x.push(0, 'Item')
+        try:
+            item = x.peek()
+        except IndexError:
+            return True
+        assert item[0] == 0
+
+    for include_data in [True, False]:
+        yield test, include_data
+
+def test_clear():
+    x = DDPQ()
+    for _ in xrange(5):
+        x.push(random(), 'Item')
+    assert len(x) == 5
+    x.clear()
+    assert len(x) == 0
+
+# if __name__ == '__main__':
+#     tests = [
+#         test_priority,
+#         test_priority_static,
+#         test_priority_dynamic,
+#         test_spool_iter_to_file,
+#         test_length,
+#         test_peek,
+#         test_clear
+#     ]
+#     for test in tests:
+#         test_iter = test()
+#         if test_iter:
+#             for x in test_iter:
+#                 x[0](*x[1:])
